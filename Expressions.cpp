5826509172d420c8795a10de5b7b3f52991a2678
Kyonosuke <133789020+kyonosukec@users.noreply.github.com>
2026-09-08 05:13:49 +0000

    done

---
+++ Expressions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello COSC 1030" << endl;
+    cout << "My name is Kyonosuke Chikamoto" << endl;
+    cout << "My major is Computer Science" << endl;
+    cout << "My hobbies are Soccer and fishing" << endl;
+    cout << "In my free time I like to read manga" << endl;
+    cout << "My favorite TV Show is Steins Gate" << endl;
+    cout << "My favorite coding language is Python" << endl;
+    cout << "In the future I want to become a Professional Soccer Player" << endl;
+    cout << "My favorite manga is Blue Lock" << endl;
+    cout << "My favorite movie is Spirited Away" << endl;
+    cout << "My favorite soccer player is Pedri" << endl;
+
+    return 0;
+}
